1eafb995bad66d97f59685a969086d9621548bf7
Marko Harjula <marko.harjula@gmail.com>
2013-01-12 10:48:25 +0200

    added Font and Resource

---
+++ src/lan/sahara/jxs/common/Font.java
@@ -0,0 +1,13 @@
+package lan.sahara.jxs.common;
+
+import lan.sahara.jxs.server.Client;
+import lan.sahara.jxs.server.XServer;
+
+public class Font extends Resource {
+
+	protected Font(int type, int id, XServer xServer, Client client) {
+		super(type, id, xServer, client);
+		// TODO Auto-generated constructor stub
+	}
+
+}
